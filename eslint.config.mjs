217b924e56c0580ac7b2655ad2a3805b054694ed
Woods <woods.web3@gmail.com>
2025-03-18 17:19:39 +0800

    fix: eslint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,6 +28,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/prefer-nullish-coalescing": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
       "@typescript-eslint/consistent-type-imports": [
         "warn",
         {
